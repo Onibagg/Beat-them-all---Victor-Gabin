835632adb222eb344e191db194c08edbbd9978d7
Onibagg <gabin.deme@etudiant.univ-rennes1.fr>
2024-11-12 10:02:14 +0100

    First push
    Creation of the different class with @TyZoxx
    
    Co-Authored-By: TyZoxx <128470926+TyZoxx@users.noreply.github.com>

---
+++ Carte.java
@@ -0,0 +1,12 @@
+public class Carte  {
+    String lieu;
+    String nom;
+    int longueur;
+
+    public Carte(String lieu, String nom, int longueur) {
+        this.lieu = lieu;
+        this.nom = nom;
+        this.longueur = longueur;
+    }
+
+}
---
+++ Personnage.java
@@ -0,0 +1,17 @@
+public class Personnage {
+    int PV;
+    int force;
+    int defense;
+    String nom;
+    String arme;
+
+    public Personnage(int PV, int force, int defense, String nom, String arme) {
+        this.PV = PV;
+        this.force = force;
+        this.defense = defense;
+        this.nom = nom;
+        this.arme = arme;
+    }
+
+
+}
